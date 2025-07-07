2ccb37885015e363b02f4ff5153dc077bcf7770c
Kaytster <ktsharps@gmail.com>
2025-07-07 11:26:00 +0100

    Welcome page

--- Welcome.php
+++ Welcome.php
@@ -12,11 +12,51 @@
      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c">
      
 </head>
 <body>
-    
+    <div class="container"> 
+
+        <div class="row align-items-start">
+
+            <div class="col-9" style="margin-left: -170px; margin-bottom: 80px"> <!-- Holds the logo -->
+                <img class="img-fluid" src="Logo-NOBG.png" alt="PlayProgress. Your gaming journey, perfectly chronicled">
+            </div>
+
+            <div class="col-3" style="margin-left: 150px; height:700px; width: 700px;"> <!-- Holds the example image -->
+                <!-- <h1> welcome </h1> -->
+                 <!-- <img class="intro img-fluid" src="Cherry.jpg"> -->
+            </div>
+
+        </div>
+
+        <div class="row align-items-start" style="margin-top: -300px;"> 
+
+            <div class="col-9" style="margin-left: -170px; margin-bottom: 100px; height:100px"> <!-- Holds intro text -->
+                <h1> Collect Games within your Library </h1>
+                <br/>
+                <h1> Log the time spent on your Journey </h1>
+                <br/>
+                <h1> All your Achievements, in one place </h1>
+            </div>
+
+        </div>
+
+        <div class="row align-items-start">
+
+            <div class="col-9 " style="margin-left: -170px; height:100px"> <!-- Holds buttons -->
+                <div class="d-flex align-items-center justify-content-center" style="height: 100%;">
+                    <button class="btn-signup" type="submit">Signup</button>
+                    <p class="mx-4"> or </p>
+                    <button class="btn-login" type="submit">Login</button>
+                    <p class="mx-4"> today! </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
     
 
 </body>
